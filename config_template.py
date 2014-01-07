852a23ac49d3573cb570792d8897270b6e46e7a0
shepdl <shepard.david@gmail.com>
2014-01-06 20:32:14 -0800

    Configuration template.

---
+++ config_template.py
@@ -0,0 +1,13 @@
+config = {
+    "twitter_consumer_key" : "",
+    "twitter_consumer_secret" : "",
+    "twitter_oauth_token" : "",
+    "twitter_oauth_token_secret" : "",
+    "db_username" : "",
+    "db_password" : "",
+    "pg_connection_string" : "",
+    "server_socket_port" : 9001,
+    "pidfile" : "/data/log/twitter-daemon.pid",
+    "debug_log" : "/data/log/twitter-daemon-out.log",
+    "error_log" : "/data/log/twitter-daemon-err.log",
+}
